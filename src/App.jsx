eda13023cf5ff443ca98bb488b1a63144a7f6d39
yuvraj1718 <yuvraj69399@gmail.com>
2025-11-18 16:08:12 +0530

    Contact name change

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import FeatureCards from './sections/FeatureCards.jsx'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from './sections/ShowcaseSection.jsx'
 import TechStack from './sections/TechStack.jsx'
-import Contact from './sections/contact.jsx'
+import Contact from './sections/Contact.jsx'
 import Footer from './sections/Footer.jsx'
 
 const App = () =>{
